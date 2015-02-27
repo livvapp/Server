efdf55282ce7dd65c4bc76eaed22492c19cb5b6b
Liam Cardenas <liamcardenas@gmail.com>
2015-02-27 14:57:54 -0800

    Initial Commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('v5App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
